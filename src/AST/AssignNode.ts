4977f7c669fdc84b4fb300908ffcc4ed40579a67
dima92 <dima-tkachenko@mail.ru>
2021-05-12 19:20:18 +0300

    Реализация

---
+++ src/AST/AssignNode.ts
@@ -0,0 +1,16 @@
+import Token from "../Token";
+import ExpressionNode from "./ExpressionNode";
+
+export default class AssignNode extends ExpressionNode {
+    operator: Token;
+    leftNode: ExpressionNode;
+    rightNode: ExpressionNode;
+
+
+    constructor(operator: Token, leftNode: ExpressionNode, rightNode: ExpressionNode) {
+        super();
+        this.operator = operator;
+        this.leftNode = leftNode;
+        this.rightNode = rightNode;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-const str = 'ТЕСТ';
-console.log(str);
+import Lexer from "./Lexer";
+import Parser from "./Parser";
+
+const code =
+    `
+    сумма РАВНО 5 МИНУС 9;
+    суммадва РАВНО 0 МИНУС 6;
+    КОНСОЛЬ сумма;
+    КОНСОЛЬ суммадва;
+    КОНСОЛЬ сумма МИНУС суммадва ПЛЮС ( 5 ПЛЮС 3 );
+    `
+
+const lexer = new Lexer(code);
+lexer.lexAnalysis()
+
+const parser = new Parser(lexer.tokenList);
+const rootNode = parser.parseCode();
+
+parser.run(rootNode);
